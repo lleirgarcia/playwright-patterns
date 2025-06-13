260de472989ad554aa9b7a2b349d716ea5f0242e
Lleir Garcia <lleirgarcia@gmail.com>
2025-06-13 14:26:15 +0200

    show reports in pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
    reporter: [
-    ['list'],
+    ['html'],
     ['json', {  outputFile: 'test-results.json' }]
   ],
   timeout: 10 * 1000, // Set a global timeout of 10 seconds for each test
