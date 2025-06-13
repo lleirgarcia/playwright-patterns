d4fb59e5ffea547cdc13d52d36f8144bdff47d74
Lleir Garcia <lleirgarcia@gmail.com>
2025-06-13 15:56:21 +0200

    reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'test-reports' }]],
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
   timeout: 10 * 1000, // Set a global timeout of 10 seconds for each test
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
